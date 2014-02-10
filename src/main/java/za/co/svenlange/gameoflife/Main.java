d19cd419da68b9e85b7ec473225e63250c95299b
svenlange <sv3n.lange@gmail.com>
2014-02-10 18:24:59 +0100

    optimized ui rendering

--- src/main/java/za/co/svenlange/gameoflife/Main.java
+++ src/main/java/za/co/svenlange/gameoflife/Main.java
@@ -14,78 +14,79 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private static final int CELL_SIZE = 1;
-    private static final Color BACKGROUND_COLOR = Color.WHITE;
-    private static final Color CELL_COLOR = Color.ORANGERED;
+	private static final int CELL_SIZE = 1;
+	private static final Color BACKGROUND_COLOR = Color.WHITE;
+	private static final Color CELL_COLOR = Color.ORANGERED;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Conway's Game of Life by Sven Lange");
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Conway's Game of Life by Sven Lange");
 
-        Grid grid = PredefinedGrids.getActionGrid();
+		Grid grid = PredefinedGrids.getActionGrid();
 
-        final Group root = new Group();
-        root.getChildren().add(createCanvas(grid));
-        stage.setScene(new Scene(root, getSceneWidth(grid), getSceneHeight(grid), BACKGROUND_COLOR));
-        stage.show();
+		final Group root = new Group();
+		final Canvas canvas = new Canvas(getSceneWidth(grid), getSceneHeight(grid));
+		root.getChildren().add(canvas);
+		stage.setScene(new Scene(root, getSceneWidth(grid), getSceneHeight(grid), BACKGROUND_COLOR));
+		stage.show();
 
-        final GridService service = new GridService(grid);
-        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent workerStateEvent) {
-                Grid grid = (Grid) workerStateEvent.getSource().getValue();
-                root.getChildren().clear();
-                root.getChildren().add(createCanvas(grid));
-                service.restart();
-            }
-        });
-        service.start();
-    }
+		final GridService service = new GridService(grid);
+		service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent workerStateEvent) {
+				Grid grid = (Grid) workerStateEvent.getSource().getValue();
+				updateCanvas(grid, canvas);
+				service.restart();
+			}
+		});
+		service.start();
+	}
 
-    private int getSceneWidth(Grid grid) {
-        return grid.getWidth() * CELL_SIZE;
-    }
+	private int getSceneWidth(Grid grid) {
+		return grid.getWidth() * CELL_SIZE;
+	}
 
-    private int getSceneHeight(Grid grid) {
-        return grid.getHeight() * CELL_SIZE;
-    }
+	private int getSceneHeight(Grid grid) {
+		return grid.getHeight() * CELL_SIZE;
+	}
 
-    private Canvas createCanvas(Grid grid) {
-        Canvas canvas = new Canvas(getSceneWidth(grid), getSceneHeight(grid));
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+	private void updateCanvas(Grid grid, Canvas canvas) {
+		GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        // draw alive cells
-        for (Cell cell : grid.getAliveCells()) {
-            gc.setFill(CELL_COLOR);
-            gc.fillRect(cell.getX() * CELL_SIZE, cell.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-        }
+		gc.setFill(BACKGROUND_COLOR);
+		gc.fillRect(0, 0, grid.getWidth(), grid.getHeight());
 
-        return canvas;
-    }
+		// draw alive cells
+		gc.setFill(CELL_COLOR);
+		for (Cell cell : grid.getAliveCells()) {
+			gc.fillRect(cell.getX() * CELL_SIZE, cell.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+		}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	}
 
-    private class GridService extends Service<Grid> {
+	private class GridService extends Service<Grid> {
 
-        private Grid grid;
+		private Grid grid;
 
-        public GridService(Grid grid) {
-            this.grid = grid;
-        }
+		public GridService(Grid grid) {
+			this.grid = grid;
+		}
 
-        @Override
-        protected Task<Grid> createTask() {
-            return new Task<Grid>() {
+		@Override
+		protected Task<Grid> createTask() {
+			return new Task<Grid>() {
 
-                @Override
-                protected Grid call() throws Exception {
-                    Thread.sleep(100);
-                    grid = grid.getNextGeneration();
-                    return grid;
-                }
-            };
-        }
-    }
+				@Override
+				protected Grid call() throws Exception {
+					grid = grid.getNextGeneration();
+					return grid;
+				}
+			};
+		}
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }

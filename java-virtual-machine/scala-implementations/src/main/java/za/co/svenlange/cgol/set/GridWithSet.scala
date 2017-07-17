722b51529d9a7d739f16722fa8ad4507a68fff8d
Sven Lange <email@svenlange.co.za>
2017-07-17 19:31:26 +0200

    Moved extension code to grid impl class and added type information to grid impl

--- java-virtual-machine/scala-implementations/src/main/java/za/co/svenlange/cgol/set/GridWithSet.scala
+++ java-virtual-machine/scala-implementations/src/main/java/za/co/svenlange/cgol/set/GridWithSet.scala
@@ -1,6 +1,7 @@
 package za.co.svenlange.cgol.set
 
-import CellExtensions._
+import java.util
+
 import za.co.svenlange.gameoflife.{Cell, Grid}
 
 import scala.collection.JavaConverters._
@@ -12,26 +13,47 @@ class GridWithSet(aliveCells: mutable.HashSet[Cell]) extends Grid {
     this(new mutable.HashSet[Cell]())
   }
 
-  override def getNextGeneration = {
+  override def getNextGeneration: GridWithSet = {
     val survivors = aliveCells.filter(cell => getNumberOfAliveNeighbors(cell) == 2 || getNumberOfAliveNeighbors(cell) == 3)
-    val cellsThatAreReborn = aliveCells.flatMap(_.neighbors).diff(aliveCells).filter(getNumberOfAliveNeighbors(_) == 3)
+    val cellsThatAreReborn = aliveCells.flatMap(neighbors).diff(aliveCells).filter(getNumberOfAliveNeighbors(_) == 3)
     new GridWithSet(survivors ++ cellsThatAreReborn)
   }
 
-  override def getNumberOfAliveNeighbors(x: Int, y: Int) = {
+  override def getNumberOfAliveNeighbors(x: Int, y: Int): Long = {
     getNumberOfAliveNeighbors(new Cell(x, y))
   }
 
-  def getNumberOfAliveNeighbors(cell: Cell) = {
-    cell.neighbors.intersect(aliveCells).size.toLong
+  def getNumberOfAliveNeighbors(cell: Cell): Long = {
+    neighbors(cell).intersect(aliveCells).size.toLong
   }
 
-  override def isCellAlive(x: Int, y: Int) = aliveCells.contains(new Cell(x, y))
+  override def isCellAlive(x: Int, y: Int): Boolean = aliveCells.contains(new Cell(x, y))
 
-  override def addAliveCell(x: Int, y: Int) = {
+  override def addAliveCell(x: Int, y: Int): Unit = {
     aliveCells.add(new Cell(x, y))
   }
 
-  override def getAliveCells = aliveCells.asJava
+  override def getAliveCells: util.Set[Cell] = aliveCells.asJava
+
+  def neighbors(cell: Cell): Set[Cell] = {
+    val x = cell.getX
+    val y = cell.getY
+    val center: Int = 0
+    val north: Int = -1
+    val south: Int = 1
+    val west: Int = -1
+    val east: Int = 1
+
+    Set(
+      new Cell(x + west, y + north),
+      new Cell(x + center, y + north),
+      new Cell(x + east, y + north),
+      new Cell(x + west, y + center),
+      new Cell(x + east, y + center),
+      new Cell(x + east, y + south),
+      new Cell(x + center, y + south),
+      new Cell(x + west, y + south)
+    )
+  }
 
 }

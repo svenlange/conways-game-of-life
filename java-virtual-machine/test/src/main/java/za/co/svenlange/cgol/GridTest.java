ea8a48d5f249a7d7813f6670a298c7be3b286118
svenlange <email@svenlange.co.za>
2018-12-04 16:14:11 +0100

    updated junit 4 to junit 5

--- java-virtual-machine/test/src/main/java/za/co/svenlange/cgol/GridTest.java
+++ java-virtual-machine/test/src/main/java/za/co/svenlange/cgol/GridTest.java
@@ -1,11 +1,9 @@
 package za.co.svenlange.cgol;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import za.co.svenlange.gameoflife.Grid;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 abstract public class GridTest {
 
